a53735c64be77606b3d0754c8a6899742e7e2b8b
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-28 15:47:37 -0700

    rendering a 6 sided and 10 sided dice, able to render a html file

--- die_visual.py
+++ die_visual.py
@@ -32,6 +32,7 @@ labels = {"x": "die roll", "y": "frequencies"}
 fig = px.bar(x = poss_results, y = frequencies, title = title, labels = labels)
 fig.show()
 
+# Rolling 2 dice
 dice_one = Dice()
 dice_two = Dice()
 
@@ -50,4 +51,31 @@ for value in poss_results:
 title = "Results of rolling two dice"
 labels = {'x': 'Result', 'y': 'Frequency of result'}
 fig = px.bar(x = poss_results, y = frequencies, title = title, labels = labels)
+# Further customize the graph
+fig.update_layout(xaxis_dtick=1)
 fig.show()
+
+
+dice_one = Dice()
+dice_two = Dice(10)
+
+results = []
+for roll_num in range(50_000):
+    result = dice_one.roll() + dice_two.roll()
+    results.append(result)
+
+frequencies = []
+max_result = dice_one.num_sides + dice_two.num_sides
+poss_results = range(2, max_result + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+title = "Results of rolling two dice"
+labels = {'x': 'Result', 'y': 'Frequency of result'}
+fig = px.bar(x = poss_results, y = frequencies, title = title, labels = labels)
+# Further customize the graph
+fig.update_layout(xaxis_dtick=1)
+fig.show()
+fig.write_html('dice_visual_d6d10.html')
+
