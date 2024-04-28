c61300b8fca03db56bfb0db8c70d4f9ac4b3a8b3
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-27 20:22:57 -0700

    scatter plot updates -able to plot start and ending points -able to plot with increasing colors to mark start and end -able to remove axis data

--- rw_visual.py
+++ rw_visual.py
@@ -16,3 +16,33 @@ ax.set_aspect('equal')
 plt.savefig("random_walk_simulation.png", bbox_inches = 'tight' )
 
 plt.show()
+
+# Keep making new walks, as long as the program is active.
+
+while True:
+    # Make a random walk
+    rw = RandomWalk(1_000_000)
+    rw.fill_walk()
+
+    # Plot the point in the walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolors = 'none', s = 1)
+    ax.set_aspect('equal')
+
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, c = 'green', edgecolors = 'none', s = 100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolors = 'none', s = 100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    plt.savefig("random_walk_simulation.png", bbox_inches = 'tight' )
+
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
