9af19844e5d0f3a3d067901c25bc339cbe7e747a
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-04-28 17:21:52 -0700

    able to render dates as axes

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import csv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text(encoding = 'utf-8').splitlines()
@@ -7,3 +10,30 @@ lines = path.read_text(encoding = 'utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 print(header_row)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+# Extract dates and high temperatures
+dates, highs = [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    high = int(row[4])
+    dates.append(current_date)
+    highs.append(high)
+
+print(highs)
+
+# Plot the high temperatures
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color = 'red')
+
+# Format plot
+ax.set_title("Daily high temperatures, July 2021", fontsize = 24)
+ax.set_xlabel("", fontsize = 16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (f)", fontsize = 16)
+ax.tick_params(labelsize = 16)
+
+plt.show()
